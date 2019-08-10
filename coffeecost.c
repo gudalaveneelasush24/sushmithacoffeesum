0f801da5bd9fa3f5cbe3d6a28f7e0aa07c40cd9e
Gudala Veneela Sushmitha <34344314+gudalaveneelasush24@users.noreply.github.com>
2019-08-10 18:14:59 +0530

    Update coffeecost.c

--- coffeecost.c
+++ coffeecost.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 #include<conio.h>
 int main{
-int X,P,Total_Cost;
+int X,P,Total_Cost = 0;
 scanf("%d %d",&X,&P);
 while(X>0)
 {
 Total_Cost=Total_Cost+X;
 X=(P*X)/100;
 }
-printf("%d",Total_cost);
+printf("%d",Total_Cost);
 return 0;
 }
 
